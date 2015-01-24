9388f9c91be60392f62058a90f311d2f863ac1ef
mkfsn <p408865@gmail.com>
2015-01-24 15:17:07 +0000

    Create login page

---
+++ application/views/user_view/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login</title>
+</head>
+<body>
+	<form action="" method="POST">
+		Username: <input type="text" name="username" placeholder="Username">
+		<br>
+		Passowrd: <input type="password" name="password" placeholder="Password">
+		<br>
+		<input type="submit" value="Login">
+	</form>
+</body>
+</html>
