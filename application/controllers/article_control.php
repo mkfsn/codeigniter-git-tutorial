abfeb879e9758514ffcbece394420ffea7874e48
mkfsn <p408865@gmail.com>
2015-01-24 13:33:37 +0000

    Detect POST and correct data, call create article in model

--- application/controllers/article_control.php
+++ application/controllers/article_control.php
@@ -11,6 +11,14 @@ class Article_control extends CI_Controller {
 
 	public function get($id = -1)
 	{
+
+		$name = $this->input->post('name');
+		$content = $this->input->post('content');
+
+		if ($name != NULL and $content != NULL) {
+			$this->article_model->article_create($name, $content);
+		}
+
 		if ($id == -1)
 			$result = $this->article_model->article_list($id);
 		else
