622cc0b04d7109a827e325be9b53b0edcd3790ad
mkfsn <p408865@gmail.com>
2015-01-24 09:38:42 +0000

    Show articles from database

--- application/controllers/article_control.php
+++ application/controllers/article_control.php
@@ -2,12 +2,21 @@
 
 class Article_control extends CI_Controller {
 
-	public function __construct() {
+	public function __construct()
+	{
 		parent::__construct();	
+
+		$this->load->model('article_model');
 	}
 
-	public function get($id = -1) {
-		echo "article $id";
+	public function get($id = -1)
+	{
+		if ($id == -1)
+			$result = $this->article_model->article_list($id);
+		else
+			$result = $this->article_model->article_get($id);
+
+		print_r($result);
 	}
 
 };
